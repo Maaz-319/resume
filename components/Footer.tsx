919d2f8ba97a32d5aca6fff0c5c89c47d2973838
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-12-29 23:13:50 +0500

    used year auto update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,7 @@ import { socialMedia } from "@/data";
 import MagicButton from "./ui/MagicButton";
 
 const Footer = () => {
+    const year = new Date().getFullYear();
     return (
         <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
 
@@ -24,7 +25,7 @@ const Footer = () => {
             </div>
             <div className="flex mt-16 md:flex-row flex-col justify-between items-center gap-2">
                 <p className="md:text-base text-sm md:font-normal font-light">
-                    Copyright © 2025 Maaz Bin Asif
+                    Copyright © {year} Maaz Bin Asif
                 </p>
 
                 <div className="flex items-center md:gap-3 gap-6">
